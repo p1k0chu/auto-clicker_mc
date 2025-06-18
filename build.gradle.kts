c785bc63bd76bbd0744064485110ddf9334039f9
p1k0chu <anatoliyid0207@gmail.com>
2025-06-18 13:10:25 +0300

    bump minecraft version
    update gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "2.0.21"
-    id("fabric-loom") version "1.9.1"
+    id("fabric-loom") version "1.10-SNAPSHOT"
     id("com.modrinth.minotaur") version "2.+"
 }
 
